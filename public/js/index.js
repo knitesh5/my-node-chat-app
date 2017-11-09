a449c9ad36284d133065cebe75af08adf9df2e67
knitesh5 <knitesh0596@gmail.com>
2017-11-09 18:15:10 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,7 @@
-var socket = io('http://localhost:3000'); 
+        var socket = io(); 
 		socket.on('connect', function(){
 			console.log('connected to server');
-		
-
-	});
+	     });
   	
   		socket.on('disconnect',function(){
   			console.log('disconnected from server');
