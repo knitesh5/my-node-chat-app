cdd4db772204ce118851898d3bf22ac47d317835
knitesh5 <knitesh0596@gmail.com>
2017-11-13 16:10:29 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
         var socket = io(); 
+
+        function scrollToBottom(){
+          //Selectors
+          var messages=jQuery('#messages');
+          var newMessage = messages.children('li:last-child');
+          //heights
+          var clientHeight = messages.prop('clientHeight');
+          var scrollTop = messages.prop('scrollTop');
+          var scrollHeight = messages.prop('scrollHeight');
+          var newMessageHeight = newMessage.innerHeight();
+          var lastMessageHeight = newMessage.prev().innerHeight();
+
+          if(clientHeight+scrollTop+lastMessageHeight+lastMessageHeight>=scrollHeight){
+          	messages.scrollTop(scrollHeight);
+          }
+
+        }
+
 		socket.on('connect', function(){
 			console.log('connected to server');
 	     });
@@ -16,13 +34,10 @@
   				createdAt:formattedTime
   			});
 
-  			jQuery('#messages').append(html); 
+  			jQuery('#messages').append(html);
+  			scrollToBottom(); 
   			
-  			// console.log('newMessage',message);
-  			// var li= jQuery('<li></li>');
-  			// li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  			// jQuery('#messages').append(li);
+  		
   		});
 
   		socket.on('newLocationMessage',function (message){
@@ -35,6 +50,7 @@
   			}); 
 
   			jQuery('#messages').append(html);
+  			scrollToBottom();
 
   		});
         
